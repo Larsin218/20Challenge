d8ce33acccabfb90a28ec7b06baeb97bda0e59ec
Larsin Smith <larsinsmith@Kirigaya.local>
2024-06-03 19:25:30 -0600

    added images

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -5,28 +5,28 @@ const Project = () => {
         <a href="https://thenotetaker-3b3e8fdadbe3.herokuapp.com/">
           <img
             className="pic"
-            src="https://placehold.co/600x400"
+            src="../../../src/assets/images/1.png"
             alt="NoteTaker"
           />
         </a>
         <a href="https://larsin218.github.io/Password_Generator/">
           <img
             className="pic"
-            src="https://placehold.co/600x400"
+            src="../../../src/assets/images/2.png"
             alt="PasswordGenerator"
           />
         </a>
         <a href="https://larsin218.github.io/Weather_App/">
           <img
             className="pic"
-            src="https://placehold.co/600x400"
+            src="../../../src/assets/images/3.png"
             alt="WeatherApp"
           />
         </a>
         <a href="https://larsin218.github.io/Work_Day_Scheduler/">
           <img
             className="pic"
-            src="https://placehold.co/600x400"
+            src="../../../src/assets/images/4.png"
             alt="WorkDayScheduler"
           />
         </a>
